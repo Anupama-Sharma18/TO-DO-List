c67eab73c66c5e4d3e46f4cbb46d2f9561a45151
Anupama Sharma <anusharma0418@gmail.com>
2025-10-27 11:46:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+// const inputBox = document.getElementById("input-box")
+// const listContainer = document.getElementById("list-container")
+
+// function addTask(){
+//     if(inputBox.value === ""){
+//         alert("you must write something!");
+//     }else{
+//         let li = document.createElement("li");
+//         li.innerHTML = inputBox.value;
+//         listContainer.appendChild(li);
+
+//         let span = document.createElement("span");
+//         span.innerHTML = "\u00d7";
+//         li.appendChild(span)
+//         inputBox.value = "";
+
+//     }
+//     inputBox.value = "";
+//     saveData();
+// }
+
+// listContainer.addEventListener("click", function(e){
+//     if(e.target.tagName === "LI"){
+//         e.target.classList.toggle("checked");
+//         saveData();
+//     }else if(e.target.tagName === "SPAN"){
+//         e.target.parentElement.remove();
+//         saveData();
+//     }
+// },false);
+
+// function saveData(){
+//     localStorage.setItem("data", listContainer.innerHTML);
+// }
+
+// function showData(){
+//     localStorage.innerHTML = localStorage.getItem("data");
+// };
+
+// showData();
+
+
+
+
+
+
+
+
+
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"; // Unicode for '×' (delete)
+        li.appendChild(span);
+        inputBox.value = "";
+    }
+    saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showData() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showData();
